1598929c695aff449c3ee6e14ab196e7426f8722
Mohammed Rashid <mohmad.rashid@hotmail.com>
2017-10-15 20:50:14 +0400

    Version 1: Simple file choose and upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+//Extra Libraries, Need to install
+import { FileChooser } from '@ionic-native/file-chooser';
+import { FilePath } from '@ionic-native/file-path';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,72 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  uri:any;
+  fileName:any;
+  uploadStatus:any;
+  uploadMessage:any;
 
+  constructor(public navCtrl: NavController,
+    private transfer: FileTransfer,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,private fileChooser: FileChooser,private filePath: FilePath) {}
+
+  uploadFile() {
+    //Choose the file using filechoose library
+    this.fileChooser.open()
+    .then(uri => {
+      this.uri=uri;
+      return uri;
+    })
+    //Get filename using filepath library
+    .then(uri=>this.filePath.resolveNativePath(uri))
+    .then(filePath => {
+      let filename:any = filePath.split('/');
+      filename = filename[filename.length - 1];
+      return(filename)
+    })
+    //Start Transfering using file transfer plugin
+    .then(filename=>{
+      this.fileName=filename;
+      let loader = this.loadingCtrl.create({
+        content: "Uploading..."
+      });
+      loader.present();
+      const fileTransfer: FileTransferObject = this.transfer.create();
+      let options: FileUploadOptions = {
+        fileKey: 'file',
+        fileName: filename,
+        chunkedMode: false,
+        headers: {}
+      }
+      fileTransfer.upload(this.uri, 'https://api.carvice.co/file/bill', options)
+        .then((data) => {
+          this.uploadStatus=data.responseCode;
+          this.uploadMessage=JSON.stringify(data.response);
+          console.log(data+" Uploaded Successfully");
+          alert(JSON.stringify(data));
+          loader.dismiss();
+          this.presentToast("Image uploaded successfully");
+      }, (err) => {
+        console.log(err);
+        loader.dismiss();
+        this.presentToast(err);
+      });
+    })
+    .catch(err => alert(err));
+  }
+
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 6000,
+      position: 'bottom'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+    toast.present();
   }
 
 }
